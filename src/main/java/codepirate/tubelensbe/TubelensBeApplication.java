4d1793fdb3452de6989c82075f46d48beec2642d
Chaemin Yooun <cinnamein@gmail.com>
2025-04-16 11:52:36 +0900

    KAN-3 main: 프로젝트 초기 세팅 #comment 의존성 추가
    # 의존성 추가
    - jpa
    - elastic search

---
+++ src/main/java/codepirate/tubelensbe/TubelensBeApplication.java
@@ -0,0 +1,13 @@
+package codepirate.tubelensbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubelensBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubelensBeApplication.class, args);
+	}
+
+}
