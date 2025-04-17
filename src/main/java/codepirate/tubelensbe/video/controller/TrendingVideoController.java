1177d4e239cd697009c242169df27e2de0354ab6
Azamman327 <wjsdbdud327@naver.com>
2025-04-17 18:50:10 +0900

    ✨Feat: youtube api 동영상 리스트 가져오기

--- src/main/java/codepirate/tubelensbe/video/controller/TrendingVideoController.java
+++ src/main/java/codepirate/tubelensbe/video/controller/TrendingVideoController.java
@@ -1,24 +1,43 @@
 package codepirate.tubelensbe.video.controller;
 
 import codepirate.tubelensbe.video.domain.TrendingVideo;
+import codepirate.tubelensbe.video.dto.VideoParam;
 import codepirate.tubelensbe.video.repository.TrendingVideoRepository;
+import codepirate.tubelensbe.video.service.ApiService;
 import codepirate.tubelensbe.video.service.TrendingVideoService;
+import com.google.api.services.youtube.model.VideoListResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.io.IOException;
 
 @Controller
 public class TrendingVideoController {
     private final TrendingVideoService trendingVideoService;
+    private final ApiService apiService = new ApiService();
+
+    private static final Logger log = LoggerFactory.getLogger(TrendingVideoController.class);
 
     public TrendingVideoController(TrendingVideoService trendingVideoService) {
         this.trendingVideoService = trendingVideoService;
     }
 
-    @PostMapping("/videoInsert")
-    public List<TrendingVideo> videoInsert() {
-
+    @GetMapping("/video")
+    public void videoInsert(
+            @RequestParam String part,
+            @RequestParam String chart,
+            @RequestParam String regionCode,
+            @RequestParam String videoCategoryId,
+            @RequestParam Long maxResults,
+            @RequestParam String key
+    )
+            throws IOException {
+        VideoParam videoParam = new VideoParam(part, chart, regionCode, videoCategoryId, maxResults, key);
+        apiService.insertVideos(videoParam);
     }
 }
