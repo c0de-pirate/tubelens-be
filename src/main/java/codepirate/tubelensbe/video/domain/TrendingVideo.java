c4bdb4fdc28f9dbc1f0b13124d13edd008411d30
Atriel1999 <fortuny1999@gmail.com>
2025-04-17 14:04:08 +0900

    📦Chore: logstash conf파일수정, application.yaml파일 개인세팅용으로 수정

--- src/main/java/codepirate/tubelensbe/video/domain/TrendingVideo.java
+++ src/main/java/codepirate/tubelensbe/video/domain/TrendingVideo.java
@@ -1,9 +1,13 @@
 package codepirate.tubelensbe.video.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -17,4 +21,12 @@ public class TrendingVideo {
 
     @ElementCollection
     private List<String> tags;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
