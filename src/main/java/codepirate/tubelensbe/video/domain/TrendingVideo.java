245affe005d590be5021b2caaa5a6ddfb3448e54
ysang989 <67964576+ysang989@users.noreply.github.com>
2025-04-18 17:31:50 +0900

    reset

--- src/main/java/codepirate/tubelensbe/video/domain/TrendingVideo.java
+++ src/main/java/codepirate/tubelensbe/video/domain/TrendingVideo.java
@@ -3,7 +3,9 @@ package codepirate.tubelensbe.video.domain;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -13,8 +15,25 @@ public class TrendingVideo {
 
     private String title;
 
-    private String thumbnail;
+    private String thumbnails;
+
+    private String embedHtml ;
+
+    private String publishedAt ;
+
+    private String description ;
+
+    private String channelTitle ;
+
+    private String viewCount ;
+
+    private String likeCount ;
+
+    private String commentCount ;
 
     @ElementCollection
     private List<String> tags;
+
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
 }
