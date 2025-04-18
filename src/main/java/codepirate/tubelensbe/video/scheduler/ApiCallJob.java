c6a85ee9442a2e4b85908de8d32ee32e0d25bc5f
Azamman327 <wjsdbdud327@naver.com>
2025-04-18 16:00:26 +0900

    🐛Fix: youtube builder name 미설정 경고메시지 처리

--- src/main/java/codepirate/tubelensbe/video/scheduler/ApiCallJob.java
+++ src/main/java/codepirate/tubelensbe/video/scheduler/ApiCallJob.java
@@ -3,6 +3,7 @@ package codepirate.tubelensbe.video.scheduler;
 import codepirate.tubelensbe.video.dto.VideoParam;
 import codepirate.tubelensbe.video.service.ApiService;
 import codepirate.tubelensbe.video.service.TrendingVideoService;
+import com.google.api.client.util.Value;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -22,20 +23,21 @@ public class ApiCallJob implements Job {
     private static final Logger log = LoggerFactory.getLogger(ApiService.class);
     private final ApiService apiService;
 
+    @Value("${youtube.api.key}")
+    private String youtubeApiKey;
+
     public ApiCallJob(ApiService apiService) {
         this.apiService = apiService;
     }
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        System.out.println("API 호출 실행됨");
+
         List<String> categoryId = List.of(
                 "1", "2", "10", "15", "17", "18", "19", "20", "21", "22", "23", "24",
                 "25", "26", "27", "28", "30", "31", "32", "33", "34", "35", "36",
                 "37", "38", "39", "40", "41", "42", "43", "44"
         );
-
-        log.info("API 호출 실행됨 by quartz");
 
         for (String id : categoryId) {
             VideoParam videoParam = new VideoParam(
@@ -44,10 +46,11 @@ public class ApiCallJob implements Job {
                     "KR",
                     id,
                     50L,
-                    "key"
+                    youtubeApiKey
             );
 
             try {
+                log.info("id: " + id);
                 apiService.insertVideos(videoParam);
             } catch (HttpClientErrorException e) {
                 if (e.getStatusCode() == HttpStatus.BAD_REQUEST) {
