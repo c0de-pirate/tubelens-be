d662aaac79be21ca0ccdf09e64459f1ea1e05648
Azamman327 <wjsdbdud327@naver.com>
2025-04-18 10:47:10 +0900

    ♻️Refactor: VideoParam의 getter, setter, 생성자 삭제

--- src/main/java/codepirate/tubelensbe/video/dto/VideoParam.java
+++ src/main/java/codepirate/tubelensbe/video/dto/VideoParam.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-//@AllArgsConstructor
+@AllArgsConstructor
 public class VideoParam {
     private String part;
     private String chart;
@@ -16,61 +16,4 @@ public class VideoParam {
     private String videoCategoryId;
     private Long maxResults;
     private String key;
-
-    public VideoParam(String part, String chart, String regionCode, String videoCategoryId, Long maxResults, String key) {
-        this.part = part;
-        this.chart = chart;
-        this.regionCode = regionCode;
-        this.videoCategoryId = videoCategoryId;
-        this.maxResults = maxResults;
-        this.key = key;
-    }
-
-    public String getPart() {
-        return part;
-    }
-
-    public void setPart(String part) {
-        this.part = part;
-    }
-
-    public String getChart() {
-        return chart;
-    }
-
-    public void setChart(String chart) {
-        this.chart = chart;
-    }
-
-    public String getRegionCode() {
-        return regionCode;
-    }
-
-    public void setRegionCode(String regionCode) {
-        this.regionCode = regionCode;
-    }
-
-    public String getVideoCategoryId() {
-        return videoCategoryId;
-    }
-
-    public void setVideoCategoryId(String videoCategoryId) {
-        this.videoCategoryId = videoCategoryId;
-    }
-
-    public Long getMaxResults() {
-        return maxResults;
-    }
-
-    public void setMaxResults(Long maxResults) {
-        this.maxResults = maxResults;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
 }
