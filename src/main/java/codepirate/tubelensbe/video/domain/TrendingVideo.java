24a297f2ee1ad55ab3116032f983b8043ee1ace6
yumin1209 <yumin991209@naver.com>
2025-04-20 00:46:53 +0900

    ✨Feat: 기본 검색 기능, 오타 자동 검색 기능 구현

--- src/main/java/codepirate/tubelensbe/video/domain/TrendingVideo.java
+++ src/main/java/codepirate/tubelensbe/video/domain/TrendingVideo.java
@@ -1,15 +1,23 @@
 package codepirate.tubelensbe.video.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.google.api.client.util.DateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import java.math.BigInteger;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 public class TrendingVideo {
     @Id
@@ -20,18 +28,22 @@ public class TrendingVideo {
     private String thumbnails;
 
     private String embedHtml ;
-    
-    private String publishedAt ;
-    
+
+    @Column(name = "published_at")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssX", timezone = "UTC")
+    private OffsetDateTime publishedAt;
+
+
+    @Column(length = 2000)
     private String description ;
-    
+
     private String channelTitle ;
-    
-    private String viewCount ;
-    
-    private String likeCount ;
-    
-    private String commentCount ;
+
+    private BigInteger viewCount ;
+
+    private BigInteger likeCount ;
+
+    private BigInteger commentCount ;
 
     @ElementCollection
     private List<String> tags;
