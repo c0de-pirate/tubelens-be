bf4fe4285c130338b29f21ebe9cc583cfa418af0
yumin1209 <yumin991209@naver.com>
2025-04-22 15:14:29 +0900

    ✨Feat:입력한 키워드 기반 접두사 검색 기능 구현

---
+++ src/main/java/codepirate/tubelensbe/search/service/VideoSearchService.java
@@ -0,0 +1,33 @@
+package codepirate.tubelensbe.search.service;
+
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
+import codepirate.tubelensbe.search.domain.VideoSearch;
+import codepirate.tubelensbe.search.repository.VideoSearchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static co.elastic.clients.elasticsearch._types.SortOrder.Desc;
+
+@Service
+public class VideoSearchService {
+
+    private final VideoSearchRepository videoSearchRepository;
+
+    @Autowired
+    public VideoSearchService(VideoSearchRepository videoSearchRepository) {
+        this.videoSearchRepository = videoSearchRepository;
+    }
+
+    public List<String> searchByPrefixSortedByViewCount(String prefix) {
+        return videoSearchRepository.searchByPrefixSortedByViewCount(prefix);
+    }
+}
+
