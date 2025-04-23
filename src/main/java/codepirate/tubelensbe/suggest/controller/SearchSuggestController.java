0c66bf115ec9e72e1a74bb98f822cd7412de81a3
yumin1209 <yumin991209@naver.com>
2025-04-24 01:02:04 +0900

    ✨Feat:검색 추천 키워드 기능 구현

--- src/main/java/codepirate/tubelensbe/suggest/controller/SearchSuggestController.java
+++ src/main/java/codepirate/tubelensbe/suggest/controller/SearchSuggestController.java
@@ -1,6 +1,5 @@
 package codepirate.tubelensbe.suggest.controller;
 
-import codepirate.tubelensbe.suggest.dto.SuggestionGroup;
 import codepirate.tubelensbe.suggest.service.SearchSuggestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +21,7 @@ public class SearchSuggestController {
     }
 
     @GetMapping("/suggest")
-    public List<SuggestionGroup> getSuggestions(@RequestParam String keyword) throws IOException {
+    public List<List<String>> getSuggestions(@RequestParam String keyword) throws IOException {
         return searchService.suggest(keyword);
     }
 }
