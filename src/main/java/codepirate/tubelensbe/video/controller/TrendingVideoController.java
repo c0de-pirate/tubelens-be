6c786ec78da0ce37883f90db6a7b6b80b3e2ba3e
Azamman327 <wjsdbdud327@naver.com>
2025-04-23 12:17:18 +0900

    ✨Feat: ES Spring 쿼리 사용하여 연결

--- src/main/java/codepirate/tubelensbe/video/controller/TrendingVideoController.java
+++ src/main/java/codepirate/tubelensbe/video/controller/TrendingVideoController.java
@@ -1,59 +1,34 @@
 package codepirate.tubelensbe.video.controller;
 
-import codepirate.tubelensbe.video.domain.TrendingVideo;
-import codepirate.tubelensbe.video.dto.VideoParam;
-import codepirate.tubelensbe.video.repository.TrendingVideoRepository;
-import codepirate.tubelensbe.video.service.ApiService;
 import codepirate.tubelensbe.video.service.TrendingVideoService;
-import com.google.api.services.youtube.model.VideoListResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
 
+
 @Controller
+@RequestMapping("/videos")
 public class TrendingVideoController {
     private final TrendingVideoService trendingVideoService;
-    private final ApiService apiService;
+
+    public TrendingVideoController(TrendingVideoService trendingVideoService) {
+        this.trendingVideoService = trendingVideoService;
+    }
 
     private static final Logger log = LoggerFactory.getLogger(TrendingVideoController.class);
 
-    public TrendingVideoController(TrendingVideoService trendingVideoService, ApiService apiService) {
-        this.trendingVideoService = trendingVideoService;
-        this.apiService = apiService;
-
-    }
-
-    @Value("${youtube.api.key}")
-    private String youtubeApiKey;
-
-    @GetMapping("/videos")
-    @ResponseBody
-    public void videoInsert(
-            @RequestParam String part,
-            @RequestParam String chart,
-            @RequestParam String regionCode,
-            @RequestParam String videoCategoryId,
-            @RequestParam Long maxResults,
-            @RequestParam String key
-    )
-            throws IOException {
-        VideoParam videoParam = new VideoParam(part, chart, regionCode, videoCategoryId, maxResults, key);
-        log.info(String.valueOf(maxResults));
-        apiService.insertVideos(videoParam);
-    }
-
-    @GetMapping("/api")
-    public void apitest() {
-        log.info(youtubeApiKey);
+    @GetMapping("/recomm")
+//    @ResponseBody
+    public void videoInsert(@RequestParam String videoid) throws IOException {
+        List<String> idlist = new ArrayList<>();
+        idlist.add(videoid);
+        trendingVideoService.recommVideos(idlist);
     }
 }
 
--- src/main/java/codepirate/tubelensbe/video/service/TrendingVideoService.java
+++ src/main/java/codepirate/tubelensbe/video/service/TrendingVideoService.java
@@ -1,20 +1,52 @@
 package codepirate.tubelensbe.video.service;
 
-import codepirate.tubelensbe.video.domain.TrendingVideo;
+import codepirate.tubelensbe.video.controller.TrendingVideoController;
+import codepirate.tubelensbe.video.repository.TrendingVideoESRepository;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.VideoListResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import codepirate.tubelensbe.video.repository.TrendingVideoRepository;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class TrendingVideoService {
-    private final TrendingVideoRepository trendingVideoRepository;
+    private final TrendingVideoESRepository trendingVideoESRepository;
 
-    public TrendingVideoService(TrendingVideoRepository trendingVideoRepository) {
-        this.trendingVideoRepository = trendingVideoRepository;
+    private static final Logger log = LoggerFactory.getLogger(TrendingVideoController.class);
+
+    public TrendingVideoService(TrendingVideoESRepository trendingVideoESRepository) {
+        this.trendingVideoESRepository = trendingVideoESRepository;
     }
 
-    private void saveAll(List<TrendingVideo> trendingVideos) {
-        trendingVideoRepository.saveAll(trendingVideos);
+    @Value("${youtube.api.key}")
+    private String youtubeApiKey;
+
+    public void recommVideos(List<String> id) throws IOException {
+        //제목 추출 후 repository로
+        JsonFactory jsonFactory = new JacksonFactory();
+
+        YouTube youtube = new YouTube.Builder(
+                new NetHttpTransport(),
+                jsonFactory,
+                request -> {})
+                .setApplicationName("tubelens")
+                .build();
+
+        YouTube.Videos.List video = youtube.videos().list(Collections.singletonList("snippet,statistics"));
+
+        video.setId(id);
+        video.setKey(youtubeApiKey);
+
+        VideoListResponse videoListResponse = video.execute();
+        String videoid = videoListResponse.getItems().get(0).getId();
+        trendingVideoESRepository.recommendVideosByTitleVectors(videoid);
     }
 }
